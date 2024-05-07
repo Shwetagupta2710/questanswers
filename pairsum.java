7a87d43f40a5c2d0f7c6d1a3513a7be03eb7136a
Shweta Gupta <133143856+Shwetagupta2710@users.noreply.github.com>
2024-05-08 01:55:46 +0530

    Create pairsum.java

---
+++ pairsum.java
@@ -0,0 +1,15 @@
+class pairsum {
+    public static void main(String[] args){
+        int arr[] = {0,-1,42,-3,1};
+        int x = -2;
+        int occurence = 0;
+        for(int i=0; i<5 && occurence == 0; i++){
+            for(int j=0; j<5 && occurence == 0; j++){
+                if(i!=j && arr[i]+arr[j] == x){
+                    occurence++;
+                }
+            }
+        }
+        System.out.println((occurence == 1) ? "found" : "does not found");
+    }  
+}
